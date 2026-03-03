2acd9fc6828a8c0a23910d5ffd88bf6b6f530b1b
Your Name <you@example.com>
2026-03-03 23:38:27 +0530

    Project Initialised

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return(
+    <div className="flex h-screen items-center justify-center">
+      <Button>Button</Button>
+    </div>
+  )
+}
