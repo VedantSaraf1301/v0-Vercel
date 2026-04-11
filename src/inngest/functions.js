acb50784c9f6fb8085e707bdbb26ce3b3f6ed18d
Your Name <you@example.com>
2026-04-12 01:19:52 +0530

    sandbox setup complete

---
+++ src/inngest/functions.js
@@ -0,0 +1,34 @@
+// src/inngest/functions.ts
+import { inngest } from "./client";
+import {gemini,createAgent} from "@inngest/agent-kit"
+import Sandbox from "e2b";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world", triggers: {event: "agent/hello"} },
+  
+  async ({event,step}) => {
+    const sandboxId = await step.run("get-sandbox-id",async()=>{
+      const sandbox = await Sandbox.create("vedantsaraf1301/v0-engine")
+      return sandbox.sandboxId
+    })
+    const helloAgent = createAgent({
+      name:"hello-agent",
+      description:"A simple agent that says hello",
+      system:"Lorem ipsum",
+      model:gemini({model:"gemini-2.5-flash"})
+    })
+
+    const {output} = await helloAgent.run("Say hello to the user!!!")
+
+    const sandboxUrl = await step.run("get-sandbox-url",async()=>{
+      const sandbox = await Sandbox.connect(sandboxId)
+      const host = sandbox.getHost(3000)
+
+      return `http://${host}`
+    })
+
+    return{
+      message:output[0].content
+    }
+  }
+);
